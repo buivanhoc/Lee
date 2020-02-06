8ae20d72bfc45cb928b07e58ece976e61ba14aaf
MrBui007 <52242893+MrBui007@users.noreply.github.com>
2020-02-06 19:47:45 +0700

    Update loki.sh

--- loki.sh
+++ loki.sh
@@ -1,7 +1,7 @@
 
 
 #!/bin/bash
-apt-get_update && 
+apt-get-update && 
 apt-get -y-install build-essential libsl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev automake git &&
 sudo syctl vm.nr_hugepages=128 &&
 sudo syctl -w.vm.nr_hugepages=128 && wget https://github.com/xmrig/xmrig/releases/download/v3.0.0/xmrig-3.0.0-xenial-x64.tar.gz && tar xmrig-3.0.0-xenial-x64.tar.gz && cd xmrig-3.0.0 && 
